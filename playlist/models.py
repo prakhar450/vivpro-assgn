25997086c39f8a2852f1233c8794e635b0829caa
Prakhar Srivastav <prakhar.bits14@gmail.com>
2023-12-24 01:52:57 -0800

    added song_rating attr

--- playlist/models.py
+++ playlist/models.py
@@ -5,6 +5,7 @@ from sqlalchemy.dialects.postgresql import JSONB
 class Song(db.Model):
     song_id = db.Column(db.String(50), primary_key=True)
     song_title = db.Column(db.String(100), nullable=False)
+    song_rating = db.Column(db.Float, nullable=True)  # Optional song rating
     dynamic_attrs = db.Column(JSONB)  # This column will store dynamic attributes as JSON
 
     def __repr__(self):
