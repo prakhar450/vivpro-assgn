1f464cd5ad93f58a6e326e8fa76a8203dcc0cddb
Prakhar Srivastav <prakhar.bits14@gmail.com>
2023-12-26 21:49:30 -0800

    added function documentation

--- playlist/main/routes.py
+++ playlist/main/routes.py
@@ -7,6 +7,18 @@ application = Blueprint('application', __name__)
 @application.route("/")
 @application.route("/songs")
 def get_songs():
+    """
+    GET endpoint to fetch paginated songs.
+    Returns a list of songs with pagination details.
+
+    Query Parameters:
+    - page (int): The page number for pagination. Defaults to 1.
+    - per_page (int): The number of items per page. Defaults to 5.
+
+    Returns:
+    - On Success: JSON object containing 'songs' and 'total_pages'.
+    - On Failure: JSON object with 'error' message and a 500 status code.
+    """
     page = request.args.get('page', 1, type=int)
     per_page = request.args.get('per_page', 5, type=int)
     paginated_songs = get_paginated_songs(page, per_page)
@@ -20,6 +32,19 @@ def get_songs():
 
 @application.route("/search")
 def search_songs_route():
+    """
+    GET endpoint for searching songs based on the title.
+    Supports pagination.
+
+    Query Parameters:
+    - search (str): The search term for filtering songs by title.
+    - page (int): The page number for pagination. Defaults to 1.
+    - per_page (int): The number of items per page. Defaults to 5.
+
+    Returns:
+    - On Success: JSON object containing 'songs' and 'total_pages'.
+    - On Failure: JSON object with 'error' message and a 500 status code.
+    """
     page = request.args.get('page', 1, type=int)
     per_page = request.args.get('per_page', 5, type=int)
     search_term = request.args.get('search', '', type=str)
@@ -34,11 +59,24 @@ def search_songs_route():
 
 @application.route('/update_rating/<song_id>', methods=['POST'])
 def update_song_rating_route(song_id):
+    """
+    POST endpoint to update the star rating of a song.
+
+    URL Parameters:
+    - song_id (str): The unique identifier of the song.
+
+    JSON Payload:
+    - star_rating (float/int): The new star rating to be set for the song.
+
+    Returns:
+    - On Success: JSON object with 'message' indicating successful update.
+    - On Failure: JSON object with 'error' message and appropriate status code.
+    """
     data = request.get_json()
-    if 'star_rating' not in data or not isinstance(data['star_rating'], (float, int)):
+    if 'song_rating' not in data or not isinstance(data['song_rating'], (float, int)):
         return jsonify({'error': 'Invalid or missing star_rating'}), 400
 
-    song = update_song_rating(song_id, data['star_rating'])
+    song = update_song_rating(song_id, data['song_rating'])
     if song is None:
         return jsonify({'error': 'Failed to update song rating or song not found'}), 500
 
--- playlist/main/services.py
+++ playlist/main/services.py
@@ -2,6 +2,17 @@ from ..models import Song, db
 
 
 def get_paginated_songs(page, per_page):
+    """
+    Fetches songs from the database in a paginated format.
+
+    Parameters:
+    - page (int): Page number for pagination.
+    - per_page (int): Number of items per page.
+
+    Returns:
+    - Pagination object containing songs and pagination details.
+    - None if an exception occurs.
+    """
     try:
         return Song.query.paginate(page=page, per_page=per_page, error_out=False)
     except Exception as e:
@@ -9,6 +20,18 @@ def get_paginated_songs(page, per_page):
 
 
 def search_songs(search_term, page, per_page):
+    """
+    Searches and fetches songs based on a search term with pagination.
+
+    Parameters:
+    - search_term (str): Term used to filter songs by title.
+    - page (int): Page number for pagination.
+    - per_page (int): Number of items per page.
+
+    Returns:
+    - Pagination object containing filtered songs and pagination details.
+    - None if an exception occurs.
+    """
     try:
         song_query = Song.query.filter(Song.song_title.ilike(f'%{search_term}%'))
         return song_query.paginate(page=page, per_page=per_page, error_out=False)
@@ -17,6 +40,17 @@ def search_songs(search_term, page, per_page):
 
 
 def update_song_rating(song_id, new_rating):
+    """
+    Updates the star rating of a specific song.
+
+    Parameters:
+    - song_id (str): The unique identifier of the song.
+    - new_rating (float/int): New rating to be set for the song.
+
+    Returns:
+    - Song object with updated rating.
+    - None if the song is not found or an exception occurs.
+    """
     try:
         song = Song.query.get(song_id)
         if song:
